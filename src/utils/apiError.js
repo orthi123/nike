c75fae293504444a4b8cac944e01ce97493a7bdf
orthi123 <orthi123@gmail.com>
2025-06-18 21:39:35 +0600

    apiError & apiSuccess created

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,12 +1,24 @@
 class ApiError extends Error {
-  constructor(statuscCode, message,errors) {
-    super(message); //override kore
-    this.success = false;
-      this.statuscCode = statuscCode;
-      this.message = message??"something went wrong";
-      this.errors =errors??{};
-      this.stack=this.constructor.name; //terminal e dekhi kun line a error hoyeche
+  //  @param {number} statuscode -the HTTP status code for the error
+  //  @param {string} message  -the error message
+  //  @param {object} erros -additional error details (optional)
 
+  constructor(
+    statusCode,
+    message = 'Something went wrong',
+    errors = {},
+    stack = '',
+    errorCode = ''
+  ) {
+    super(message); //override kore
+
+    (this.name=this.constructor.name),
+    this.success = false;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.errorCode = errorCode;
+    //terminal e dekhi kun line a error hoyeche
 
     //   if(process.env.NODE_ENV='development'){
     //     this.stack=this.stack;
@@ -14,10 +26,94 @@ class ApiError extends Error {
     //     this.stack=null;
     //   }
 
-   //arekta way ache-------
 
+
+    //arekta way ache-------
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
     // this.stack=Error.captureStackTrace(this,this.constructor);
+  }
 
+
+  
+
+  //401 Unauthorized-----------------
+  static unauthorized(message = 'Unauthorizes', errors = {}, errorCode = 'AUTH_401') {
+    return new ApiError(401, message, errors, '', errorCode);
+  }
+
+
+
+
+  //403 Forbidden-------------------
+  static forbidden(message = 'Forbidden', errors = {}, errorCode = 'AUTH_403') {
+    return new ApiError(403, message, errors, '', errorCode);
+  }
+
+
+
+
+  //400 Bad Request------------------------
+  static badRequest(message = 'Bad Request', error = {}, errorCode = 'REQ_400') {
+    return new ApiError(400, message, errors, '', errorCode);
+  }
+
+
+
+
+
+  //404 Not Found--------------------------
+  static notFound(message = 'Not Found', errors = {}, errorCode = 'REQ_404') {
+    return new ApiError(404, message, error, '', errorCode);
+  }
+
+
+
+
+
+  //409 Conflict---------------------------
+  static conflict(message = 'Conflict', errors = {}, errorCode = 'REQ_409') {
+    return new ApiError(409, message, errors, '', errorCode);
+  }
+
+
+
+
+
+
+  //500 Server Error--------------------------
+  static serverError(message = 'Internal Server Error', errors = {}, errorCode = 'SERVER_500') {
+    return new ApiError(500, message, error, '', errorCode);
+  }
+
+
+
+
+
+  //200 No Content---------------------------
+  static noContent(message = 'No Content') {
+    return new ApiError(204, message);
+  }
+
+
+
+
+
+  //Database Error----------------------------
+  static serverError(message = 'Database Error', errors = {}, errorCode = 'DB_500') {
+    return new ApiError(500, message, errors, '', errorCode);
+  }
+
+
+
+
+
+  //Custom
+  static custom(statusCode, message, errors = {}, errorCode = 'REQ_409') {
+    return new ApiError(statusCode, message, error, '', errorCode);
   }
 }
 
--- src/utils/apiSuccess.js
+++ src/utils/apiSuccess.js
@@ -1,13 +1,37 @@
 class ApiSuccess{
 
-constructor(statusCode,message,data={}){
+constructor(statusCode,message='Success',data={},meta={}){
     this.success=statusCode<400;
     this.statusCode=statusCode;
     this.message=message;
     this.data=data;
+    this.meta=meta;
 }
 
+//200 ok------------
 
+static ok(data={},message='OK',meta={}){
 
+    return new ApiSuccess(200,message,data,meta);
+}
+
+//201 created------------
+static created(data={},message='RESOURCE CREATED',meta={}){
+
+    return new ApiSuccess(201,message,data,meta);
+}
+
+//204 No Content(rare,data will be empty)--------------
+
+static noContent(message='OK',meta={}){
+
+    return new ApiSuccess(204,message,meta);
+}
+
+//Custom success------------
+static custom(statusCode,message,data={},meta={}){
+
+    return new ApiSuccess(statusCode,message,data,meta);
+}
 }
 export default ApiSuccess;
